793397de0e2401793c5e0e2418dfe142e7312b51
mindae <mahendrachaurasia@gmail.com>
2024-05-29 19:30:05 +0530

    added test 1

--- hello/src/main/java/hello/Main.java
+++ hello/src/main/java/hello/Main.java
@@ -1,16 +1,18 @@
 package hello;
 
 public class Main {
-public static void main(String...args) {
-SalesData data = new SalesData();
-data.display();
-	displayGreeting();
-	data.display();
+	public static void main(String... args) {
+		SalesData data = new SalesData();
+		data.display();
+		displayGreeting();
+		data.display();
 
-}
-private static void displayGreeting() {
-	System.out.println("HELLO HAPPY SALES PEOPLE");
-	System.out.println("THIS APP SHOWS SALES DATA");
-	System.out.println("Woot, push worked!");
-}
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		System.out.println("Woot, push worked!");
+		System.out.println("test 1");
+	}
 }
