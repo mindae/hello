6b5271ec9c5f96d4a8759457a313fd6e34c2e9e7
mindae <mahendrachaurasia@gmail.com>
2024-05-29 15:25:26 +0530

    initial commit

---
+++ hello/src/main/java/hello/Main.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Main {
+public static void main(String...args) {
+SalesData data = new SalesData();
+data.display();
+	displayGreeting();
+	data.display();
+
+}
+private static void displayGreeting() {
+	System.out.println("HELLO HAPPY SALES PEOPLE");
+	System.out.println("THIS APP SHOWS SALES DATA");
+}
+}
